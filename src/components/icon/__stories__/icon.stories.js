c4bd1c3fc0abc8da0c185ce6ef0f7680b9628040
Olga Popova <popova_oa@sima-land.ru>
2019-06-03 17:46:30 +0500

    #13 Описать варианты использования компонента иконки в storybook проекта

--- src/components/icon/__stories__/icon.stories.js
+++ src/components/icon/__stories__/icon.stories.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Icon from '../';
 import heart from '../../icons/heart.svg';
+import cross from '../../icons/cross.svg';
+
+const props = {
+  icon: cross,
+  size: 40,
+  color: 'dark-blue',
+};
 
 storiesOf('Icon', module)
   .add('default icon', () => <Icon/>)
-  .add('icon with text', () => (
+  .add('ordinary icon', () => <Icon {...props} />)
+  .add('icon in text', () => (
     <p
       style={{
         color: '#ff0000',
         fontSize: 20,
       }}
     >
-      We <Icon icon={heart}/> frontend!
+      We <Icon icon={heart} inline/> frontend!
     </p>
   ));
 
