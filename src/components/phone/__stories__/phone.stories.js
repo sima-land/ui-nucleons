8c9dacaa1427cb3200617cb6e91106403ba23f4d
Ekaterina <zyuzeva_ev@ekb.sima-land.ru>
2019-06-25 18:44:36 +0500

    feat(core) Добавлен хелпер для форматирования дат

--- src/components/phone/__stories__/phone.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Phone from '../';
-
-
-storiesOf('Phone', module)
-  .add('with phone number', () => <Phone phone='+7-900-800-70-60'></Phone>);
