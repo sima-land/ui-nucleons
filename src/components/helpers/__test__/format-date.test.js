61a0827987b9f72a5fd88914c2ed6266a33c50e1
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2019-07-23 16:05:34 +0500

    feat(createDateFormatter): Добавлена возможность задавать динамический формат даты через функцию.

--- src/components/helpers/__test__/format-date.test.js
+++ src/components/helpers/__test__/format-date.test.js
@@ -1,33 +1,38 @@
 import formatDate, { createDateFormatter } from '../format-date';
 
-let date;
-
 describe('formatDate()', () => {
   it('returns empty string if have been passed wrong params', () => {
-    date = formatDate();
-    expect(date).toEqual('');
+    let date = formatDate();
+    expect(date).toBe('');
     date = formatDate('2014*83');
-    expect(date).toEqual('');
+    expect(date).toBe('');
     date = formatDate('2019-15-03');
-    expect(date).toEqual('');
+    expect(date).toBe('');
   });
   it('returns correct date with default formats', () => {
-    date = formatDate('2014-02-03');
-    expect(date).toEqual('03 февраля 2014');
+    const date = formatDate('2014-02-03');
+    expect(date).toBe('03 февраля 2014');
   });
 });
 
 describe('createDateFormatter', () => {
   it('works with default format', () => {
-    date = createDateFormatter()('2019-01-03');
-    expect(date).toEqual('03 января 2019');
+    const date = createDateFormatter()('2019-01-03');
+    expect(date).toBe('03 января 2019');
   });
   it('returns correct date with custom formats', () => {
-    date = createDateFormatter('YYYY-DD-MM', 'Do MMMM')('2014-02-03');
-    expect(date).toEqual('2-го марта');
+    let date = createDateFormatter('YYYY-DD-MM', 'Do MMMM')('2014-02-03');
+    expect(date).toBe('2-го марта');
     date = createDateFormatter('YYYY-MM-DD', 'Do MMMM')('2014-02-03');
-    expect(date).toEqual('3-го февраля');
+    expect(date).toBe('3-го февраля');
     date = createDateFormatter('YYYY-MM-DD', 'dddd: D MMM')('2014-02-03');
-    expect(date).toEqual('понедельник: 3 февр.');
+    expect(date).toBe('понедельник: 3 февр.');
+  });
+  it('should handle function as second argument', () => {
+    const spy = jest.fn(moment => moment.isValid() && 'DD.MM');
+    const date = createDateFormatter('YYYY-MM-DD', spy)('2014-02-03');
+    expect(date).toBe('03.02');
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy.mock.calls[0][0].format('YYYY.MM.DD')).toBe('2014.02.03');
   });
 });
--- src/components/helpers/format-date.js
+++ src/components/helpers/format-date.js
@@ -1,18 +1,31 @@
 import moment from 'moment';
 import 'moment/locale/ru.js';
+import isFunction from 'lodash.isfunction';
 
 /**
  * Создает функцию для форматирования даты.
- * @param {string|Array} formatFrom Формат(ы) передаваемой даты.
- * @param {string} formatTo Формат возвращаемой даты.
+ * @param {string|Array<string>} formatFrom Формат(ы) принимаемой даты.
+ * @param {string|function(moment): string} formatTo Формат возвращаемой даты, переданная функция примет moment-объект.
  * @return {Function} Функция для форматирования даты.
  */
 export const createDateFormatter = (
   formatFrom = ['YYYY-MM-DD', 'DD.MM.YYYY'],
   formatTo = 'DD MMMM YYYY'
-) => value => {
-  const date = value && moment(value, formatFrom);
-  return date && date.isValid() ? date.format(formatTo) : '';
+) => {
+  const hasFormatFunctionType = isFunction(formatTo);
+
+  return value => {
+    const date = value && moment(value, formatFrom);
+    let result = '';
+
+    if (date && date.isValid()) {
+      result = date.format(
+        hasFormatFunctionType ? formatTo(date.clone()) : formatTo
+      );
+    }
+
+    return result;
+  };
 };
 
 export default createDateFormatter();
