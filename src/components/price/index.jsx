a659383fa9ea33064eeac663d23a992921833cd2
Evgeniy Labaznikov <labaznikov_e@sima-land.ru>
2019-08-05 14:16:45 +0500

    feat(Price): Добавлена возможность стилизовать дробную часть и графему валюты отдельно.

--- src/components/price/index.jsx
+++ src/components/price/index.jsx
@@ -4,10 +4,14 @@ import Type from 'prop-types';
 import styles from './price.scss';
 import classNames from 'classnames';
 
+const cx = classNames.bind(styles);
+
 /**
  * Цена товара с указанием знака валюты.
  * @param {Object} props Параметры компонента.
- * @param {string} [props.className] Название класса.
+ * @param {string} [props.className] Класс/стили цены.
+ * @param {string} [props.currencyGraphemeClass] Класс/стили графемы валюты.
+ * @param {string} [props.fractionalClass] Класс/стили дробной части валюты.
  * @param {string} [props.currencyGrapheme] Графема валюты пользователя.
  * @param {number} props.value Цена.
  * @param {boolean} [props.withFractionalPart] Указать дробную часть.
@@ -24,20 +28,22 @@ const Price = ({
   beforePrice,
   fractionalInSuper,
   boldIntegerPart,
+  currencyGraphemeClass,
+  fractionalClass,
 }) => {
   const price = formatNumber(value);
   const integer = boldIntegerPart ? <b>{price[0]}</b> : price[0];
   const sign = currencyGrapheme && (
-    <span className={styles.grapheme}>
+    <span className={cx(styles.grapheme, currencyGraphemeClass)}>
       {beforePrice ? `${currencyGrapheme}\u00A0` : `\u00A0${currencyGrapheme}`}
     </span>
   );
   return (
-    <span className={classNames(styles.price, className)}>
+    <span className={cx('price', className)}>
       {beforePrice && sign}
       {integer}
       {withFractionalPart && (
-        fractionalInSuper ? <sup>{price[1]}</sup> : `.${price[1]}`
+        fractionalInSuper ? <sup className={fractionalClass}>{price[1]}</sup> : `.${price[1]}`
       )}
       {!beforePrice && sign}
     </span>
@@ -46,11 +52,19 @@ const Price = ({
 
 Price.propTypes = {
   /**
-   * Название класса
+   * Класс/стили цены знака валюты.
+   */
+  currencyGraphemeClass: Type.string,
+  /**
+   * Класс/стили цены дробной части.
+   */
+  fractionalClass: Type.string,
+  /**
+   * Класс/стили цены валюты.
    */
   className: Type.string,
   /**
-   * Графема валюты пользователя
+   * Графема валюты пользователя.
    */
   currencyGrapheme: Type.string,
   /**
@@ -62,15 +76,15 @@ Price.propTypes = {
    */
   withFractionalPart: Type.bool,
   /**
-   * Указывать знак валюты перед ценой
+   * Указывать знак валюты перед ценой.
    */
   beforePrice: Type.bool,
   /**
-   * Указывать дробную часть цены сверху (в <sup></sup>)
+   * Указывать дробную часть цены сверху (в <sup></sup>).
    */
   fractionalInSuper: Type.bool,
   /**
-   * Выделять жирным шрифтом целую часть цены
+   * Выделять жирным шрифтом целую часть цены.
    */
   boldIntegerPart: Type.bool,
 };
