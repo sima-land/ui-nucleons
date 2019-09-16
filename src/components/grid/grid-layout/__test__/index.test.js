82a33c35babdee0239c0b6c373d92de552e102a5
Roman Otstavnov <otstavnov_rv@ekb.sima-land.ru>
2019-09-16 15:12:04 +0500

    #53 Поправить компонент GridLayout

--- src/components/grid/grid-layout/__test__/index.test.js
+++ src/components/grid/grid-layout/__test__/index.test.js
@@ -4,9 +4,28 @@ import GridLayout from '../';
 
 describe('<GridLayout/>', () => {
   it('should renders correctly without props', () => {
-    const gridLayout = shallow(<GridLayout><div className='test-content'>123</div></GridLayout>);
+    const gridLayout = shallow(<GridLayout>
+      <div className='test-content'>123</div>
+    </GridLayout>);
     expect(gridLayout.find('.layout')).toHaveLength(1);
     expect(gridLayout.find('.test-content')).toHaveLength(1);
+    expect(gridLayout).toMatchSnapshot();
+  });
+  it('should renders correctly with props', () => {
+    const gridLayout = shallow(
+      <GridLayout
+        tag='section'
+        containerProps={{
+          className: 'section-class',
+          name: 'some-name',
+        }}
+      >
+        <div className='test'>some content</div>
+      </GridLayout>
+    );
+    expect(gridLayout.find('section.layout')).toHaveLength(1);
+    expect(gridLayout.find('section.section-class')).toHaveLength(1);
+    expect(gridLayout.find('.test')).toHaveLength(1);
     expect(gridLayout).toMatchSnapshot();
   });
 });
