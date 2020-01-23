91e3176f2ed84bf16a5ef079f7aa9cb68cc38221
rotanov_ps <rotanov_ps@ekb.sima-land.ru>
2020-01-23 16:27:15 +0500

    #108 Добавить возможность переносить хлебные крошки

--- src/components/breadcrumbs/items.js
+++ src/components/breadcrumbs/items.js
@@ -79,6 +79,82 @@ const items = [
       },
     ],
   },
+  {
+    name: 'Fourth Level',
+    url: '/first_level_path',
+    isActive: false,
+    items: [
+      {
+        name: 'Fourth Level Previous Sibling',
+        url: '/fourth_level_previous_sibling_path',
+        isActive: false,
+      },
+      {
+        name: 'Fourth Level',
+        url: '/fourth_level_path',
+        isActive: true,
+      },
+      {
+        name: 'Fourth Level Next Sibling',
+        url: '/fourth_level_next_sibling_path',
+        isActive: false,
+      },
+      {
+        name: 'Fourth Level Next Sibling',
+        url: '/fourth_level_next_sibling_path',
+        isActive: false,
+      },
+      {
+        name: 'Fourth Level Next Sibling',
+        url: '/Fourth_level_next_sibling_path',
+        isActive: false,
+      },
+      {
+        name: 'Fourth Level Next Sibling',
+        url: '/fourth_level_next_sibling_path',
+        isActive: false,
+      },
+      {
+        name: 'Fourth Level Next Sibling',
+        url: '/fourth_level_next_sibling_path',
+        isActive: false,
+      },
+    ],
+  },
+  {
+    name: 'Fifth Level',
+    url: '/fifth_level_path',
+    isActive: false,
+    items: [
+      {
+        name: 'Fifth Level Previous Sibling',
+        url: '/fifth_level_previous_sibling_path',
+        isActive: false,
+      },
+      {
+        name: 'Fifth Level',
+        url: '/fifth_level_path',
+        isActive: true,
+      },
+    ],
+  },
+  {
+    name: 'Sixth Level',
+    url: '/sixth_level_path',
+    isActive: true,
+    items: [
+      {
+        name: 'Sixth Level',
+        url: '/sixth_level_path',
+        isActive: true,
+      },
+      {
+        name: 'Sixth Level Next Sibling',
+        url: '/sixth_level_next_sibling_path',
+        isActive: false,
+      },
+    ],
+  },
 ];
 
 export default items;
--- src/components/breadcrumbs/readme.md
+++ src/components/breadcrumbs/readme.md
@@ -2,7 +2,7 @@
 ```jsx
 import items from './items';
 
-<Breadcrumbs items={items} />
+<Breadcrumbs items={items.slice(0, 4)} />
 ```
 
 Хлебные крошки с функциями получения имени, ссылки и списка соседних категорий
@@ -12,10 +12,20 @@ import items from './items';
 import items from './items';
 
 <Breadcrumbs
-    items={items}
+    items={items.slice(0, 4)}
     getItemName={items => items.name}
     getItemURL={items => items.url}
     getItemSiblings={items => items.items}
     isActiveItem={({ name }) => name.includes('First')}
 />
+```
+В узком блоке:
+```jsx
+import items from './items';
+
+<div style={{ width: 550, border: '1px dashed #ccc' }}>
+    <Breadcrumbs
+        items={items}
+    />
+</div>
 ```
