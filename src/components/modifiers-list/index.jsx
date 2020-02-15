ec1b65e87b5be87565a429afe1cc40b5f1235b2a
Ekaterina Zyuzeva <zyuzeva_ev@ekb.sima-land.ru>
2020-02-15 10:57:19 +0500

    feat(ModifiersList): Добавлено отображение ссылки на таблицу размеров.

--- src/components/modifiers-list/index.jsx
+++ src/components/modifiers-list/index.jsx
@@ -1,23 +1,55 @@
 import React from 'react';
 import Type from 'prop-types';
 import ModifierItem from './modifier-item/';
+import Link from '../link';
+import Icon from '../icon';
+import statement from '../icons/statement.svg';
+import classes from './modifiers-list.scss';
+import classnames from 'classnames/bind';
+
+const cx = classnames.bind(classes);
 
 /**
  * Компонент списка модификаторов.
  * @param {Object} props Свойства компонента.
  * @param {Array} props.items Массив данных модификаторов.
  * @param {string} props.currencyGrapheme Графема валюты пользователя.
+ * @param {string} props.sizesTableUrl Url таблицы размеров.
+ * @param {Object} props.wrapperProps Свойства блока-обертки.
+ * @param {Object} props.itemsContainerProps Свойства блока-обертки списка модификаторов.
  * @return {ReactElement} Компонент списка модификаторов.
  */
-const ModifiersList = ({ items, currencyGrapheme }) => (
-  <div>
-    {Array.isArray(items) && items.map((item, index) => (
-      <ModifierItem
-        key={index}
-        {...item}
-        currencyGrapheme={currencyGrapheme}
-      />
-    ))}
+const ModifiersList = ({ items, currencyGrapheme, sizesTableUrl, wrapperProps, itemsContainerProps }) => (
+  <div {...wrapperProps}>
+    {sizesTableUrl && (
+      <div className={cx('sizes-table-wrapper')}>
+        <Link
+          className={cx('sizes-table-link')}
+          href={sizesTableUrl}
+          external
+        >
+          Таблица размеров
+          <Icon
+            inline
+            icon={statement}
+            role='presentation'
+            size={24}
+            className={cx('sizes-table-icon')}
+          />
+        </Link>
+      </div>
+    )}
+    {Array.isArray(items) && (
+      <div {...itemsContainerProps}>
+        {items.map((item, index) => (
+          <ModifierItem
+            key={index}
+            {...item}
+            currencyGrapheme={currencyGrapheme}
+          />
+        ))}
+      </div>
+    )}
   </div>
 );
 
@@ -32,6 +64,21 @@ ModifiersList.propTypes = {
    * Графема валюты пользователя.
    */
   currencyGrapheme: Type.string,
+
+  /**
+   * Url таблицы размеров.
+   */
+  sizesTableUrl: Type.string,
+
+  /**
+   * Свойства блока-обертки.
+   */
+  wrapperProps: Type.object,
+
+  /**
+   * Свойства блока-обертки списка модификаторов.
+   */
+  itemsContainerProps: Type.object,
 };
 
 export default ModifiersList;
--- src/components/modifiers-list/readme.md
+++ src/components/modifiers-list/readme.md
@@ -33,3 +33,43 @@ import items from './__stories__/items.js';
     />
 </div>
 ```
+
+Список модификаторов c таблицей размеров:
+```jsx 
+import items from './__stories__/items.js';
+
+<div style={{ background: '#fff', width: '300px', margin: '0 auto' }}>
+    <ModifiersList
+      items={items.withText}
+      currencyGrapheme='€'
+      sizesTableUrl='/'
+    />
+</div>
+```
+
+Список пользовательскими пропами:
+```jsx 
+import items from './__stories__/items.js';
+import classes from './__stories__/modifiers-list.scss';
+
+<React.Fragment>
+  <div className={classes.list}>
+    <div className={classes.title}>Обертка</div>
+    <ModifiersList
+      items={[...items.withPhoto, ...items.withColor]}
+      currencyGrapheme='₽'
+      sizesTableUrl='/'
+      wrapperProps={{ className: classes.scrollable }}
+    />
+  </div>
+  <div className={classes.list}>
+    <div className={classes.title}>Контейнер</div>
+    <ModifiersList
+      items={[...items.withPhoto, ...items.withColor]}
+      currencyGrapheme='₽'
+      sizesTableUrl='/'
+      itemsContainerProps={{ className: classes.scrollable }}
+    />
+  </div>
+</React.Fragment>
+```
