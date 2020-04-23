f3e074b54815ca6cb14cd9cadd3420c50facb99f
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2020-04-23 10:32:59 +0500

    feat(Clean.Button): Реализован компонент прозрачной кнопки. feat(Claen.Group): Реализован компонент группы прозрачных кнопок.

---
+++ src/components/clean-buttons/__test__/index.test.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Clean } from '../index';
+
+describe('<Clean.Button />', () => {
+  it('should render without props', () => {
+    const wrapper = mount(<Clean.Button />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should handle props', () => {
+    const wrapper = mount(
+      <Clean.Button
+        size='s'
+        href='https://site.com'
+        children='Go to site'
+      />
+    );
+
+    expect(wrapper.find('button')).toHaveLength(0);
+    expect(wrapper.find('a')).toHaveLength(1);
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.setProps({ href: null, asLink: true });
+
+    expect(wrapper.find('button')).toHaveLength(0);
+    expect(wrapper.find('a')).toHaveLength(1);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('<Clean.Group />', () => {
+  it('should render without props', () => {
+    const wrapper = mount(<Clean.Group />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should handle props', () => {
+    const wrapper = mount(
+      <Clean.Group size='s'>
+        <Clean.Button>Foo</Clean.Button>
+        <Clean.Button>Bar</Clean.Button>
+        <Clean.Button href='https://www.abc.xyz'>XYZ</Clean.Button>
+        <button>Just button</button>
+      </Clean.Group>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
