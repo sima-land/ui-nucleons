69fd097b587fbf285a26ecdbee99f925772021ed
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2020-07-21 10:12:24 +0500

    feat(WithTooltip): Скорректированы расчеты смещения тултипа.

--- src/components/with-tooltip/utils.js
+++ src/components/with-tooltip/utils.js
@@ -1,6 +1,7 @@
 import boundsOf from '../helpers/bounds-of';
 import { getScrollParent } from '../helpers/get-scroll-parent';
 import { findOffsetParent } from '../helpers/find-offset-parent';
+import { pick } from 'lodash';
 
 const SPACES = {
   correction: 24,
@@ -8,19 +9,14 @@ const SPACES = {
   toArea: 16,
 };
 
+const rectKeys = ['top', 'left', 'bottom', 'right', 'width', 'height'];
+
 /**
  * Преобразует css-значение размера в число.
  * @param {string} cssValue Значение, например "12.23px".
  * @return {number} Число. Неожиданно, правда?
  */
 const asNumber = cssValue => parseFloat(cssValue.replace(/[A-z]/g, '')) || 0;
-
-/**
- * Возвращает представление числового значения в пикселях.
- * @param {number} value Значение.
- * @return {string} Представление.
- */
-export const asPx = value => Number.isFinite(value) ? `${value}px` : '0px';
 
 /**
  * Складывает все аргументы.
@@ -52,7 +48,7 @@ export const CanPlace = {
   ) => boundsOf(tooltipEl).height < boundsOf(holderEl).bottom - area.top - SPACES.correction,
 };
 
-export const Place = {
+export const PlaceOffset = {
   reset: element => {
     element.style.top = null;
     element.style.left = null;
@@ -62,61 +58,65 @@ export const Place = {
   onRight: (tooltipEl, holderEl) => {
     const correction = getOriginCorrection(tooltipEl);
 
-    tooltipEl.style.left = asPx(sum(
+    return sum(
       SPACES.toHolder,
       boundsOf(holderEl).right,
       correction.x,
-    ));
+    );
   },
   onLeft: (tooltipEl, holderEl) => {
     const correction = getOriginCorrection(tooltipEl);
 
-    tooltipEl.style.left = asPx(sum(
+    return sum(
       boundsOf(holderEl).left,
       -boundsOf(tooltipEl).width,
       -SPACES.toHolder,
       correction.x,
-    ));
+    );
   },
   onBottom: (tooltipEl, holderEl) => {
     const correction = getOriginCorrection(tooltipEl);
 
-    tooltipEl.style.top = asPx(sum(
+    return sum(
       boundsOf(holderEl).bottom,
       SPACES.toHolder,
       correction.y,
-    ));
+    );
   },
   onTop: (tooltipEl, holderEl) => {
     const correction = getOriginCorrection(tooltipEl);
 
-    tooltipEl.style.top = asPx(sum(
+    return sum(
       boundsOf(holderEl).top,
       -boundsOf(tooltipEl).height,
       -SPACES.toHolder,
       correction.y
-    ));
+    );
   },
 };
 
-export const Correct = {
+export const CorrectOffset = {
   vertically: (tooltipEl, holderEl, area) => {
     const tooltipBounds = boundsOf(tooltipEl);
     const holderBounds = boundsOf(holderEl);
     const correction = getOriginCorrection(tooltipEl);
 
+    let result;
+
     if (area.bottom - holderBounds.bottom > tooltipBounds.height + SPACES.correction) {
-      tooltipEl.style.top = `${correction.y + holderBounds.top}px`;
+      result = correction.y + holderBounds.top;
     } else if (holderBounds.bottom - area.top > tooltipBounds.height + SPACES.correction) {
-      tooltipEl.style.top = `${correction.y + holderBounds.bottom - tooltipBounds.height}px`;
+      result = correction.y + holderBounds.bottom - tooltipBounds.height;
     } else {
-      tooltipEl.style.top = `${correction.y + SPACES.toArea}px`;
+      result = correction.y + SPACES.toArea;
     }
+
+    return result;
   },
   horizontally: (tooltipEl, area) => {
     const correction = getOriginCorrection(tooltipEl);
 
-    tooltipEl.style.left = `${correction.x + area.width - SPACES.toArea - boundsOf(tooltipEl).width}px`;
+    return correction.x + area.width - SPACES.toArea - boundsOf(tooltipEl).width;
   },
 };
 
@@ -127,28 +127,55 @@ export const Correct = {
  */
 export const placeTooltip = (tooltipEl, holderEl) => {
   if (tooltipEl && holderEl) {
-    const tooltipScrollParent = getScrollParent(tooltipEl);
-    const area = boundsOf(tooltipScrollParent);
+    const scrollParent = getScrollParent(tooltipEl);
+    const area = getInnerRect(scrollParent);
+    const offset = { x: 0, y: 0 };
 
-    Place.reset(tooltipEl);
+    PlaceOffset.reset(tooltipEl);
 
     if (CanPlace.onRight(holderEl, tooltipEl, area)) {
-      Place.onRight(tooltipEl, holderEl);
-      Correct.vertically(tooltipEl, holderEl, area);
+      offset.x = PlaceOffset.onRight(tooltipEl, holderEl);
+      offset.y = CorrectOffset.vertically(tooltipEl, holderEl, area);
     } else if (CanPlace.onLeft(holderEl, tooltipEl, area)) {
-      Place.onLeft(tooltipEl, holderEl);
-      Correct.vertically(tooltipEl, holderEl, area);
+      offset.x = PlaceOffset.onLeft(tooltipEl, holderEl);
+      offset.y = CorrectOffset.vertically(tooltipEl, holderEl, area);
     } else if (CanPlace.onBottom(holderEl, tooltipEl, area)) {
-      Place.onBottom(tooltipEl, holderEl);
-      Correct.horizontally(tooltipEl, area);
+      offset.y = PlaceOffset.onBottom(tooltipEl, holderEl);
+      offset.x = CorrectOffset.horizontally(tooltipEl, area);
     } else if (CanPlace.onTop(holderEl, tooltipEl, area)) {
-      Place.onTop(tooltipEl, holderEl);
-      Correct.horizontally(tooltipEl, area);
+      offset.y = PlaceOffset.onTop(tooltipEl, holderEl);
+      offset.x = CorrectOffset.horizontally(tooltipEl, area);
     }
 
     tooltipEl.style.opacity = null;
     tooltipEl.style.visibility = null;
+
+    // вместо top/left/bottom/right используем translate3d, т.к. его расчет использует графический ускоритель
+    tooltipEl.style.transform = `translate3d(${offset.x}px, ${offset.y}px, 0)`;
   }
+};
+
+/**
+ * Возвращает внутреннюю доступную область блока (с учетом прокруток).
+ * @param {Element} element Элемент.
+ * @return {Object} Прямоугольник.
+ */
+export const getInnerRect = element => {
+  const area = pick(boundsOf(element), rectKeys);
+
+  if (element.scrollWidth > area.width) {
+    area.left = area.left - element.scrollLeft;
+    area.width = element.scrollWidth;
+    area.right = area.left + area.width;
+  }
+
+  if (element.scrollHeight > area.height) {
+    area.top = area.top - element.scrollTop;
+    area.height = element.scrollHeight;
+    area.bottom = area.top + area.height;
+  }
+
+  return area;
 };
 
 /**
