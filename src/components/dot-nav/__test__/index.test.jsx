4d6be91765086266b34bbcc447f10082c3eaac20
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2020-11-06 11:06:00 +0500

    feat(DotNav): Реализован компонент для точек навигации каруселей.

---
+++ src/components/dot-nav/__test__/index.test.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { mount } from 'enzyme';
+import DotNav from '..';
+import { times } from 'lodash';
+
+describe('<DotsNav />', () => {
+  it('should renders without props', () => {
+    const wrapper = mount(
+      <DotNav />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should handle props', () => {
+    const spy = jest.fn();
+
+    const wrapper = mount(
+      <DotNav
+        current={2}
+        total={12}
+        onSelect={spy}
+      />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+
+    expect(spy).toBeCalledTimes(0);
+
+    act(() => {
+      wrapper.find('.item').at(5).simulate('click');
+    });
+    wrapper.update();
+
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toBeCalledWith(5);
+  });
+
+  it('should handle "current" change', () => {
+    const wrapper = mount(
+      <DotNav
+        current={0}
+        total={9}
+      />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+
+    times(9).forEach(index => {
+      act(() => {
+        wrapper.setProps({ current: index });
+      });
+      wrapper.update();
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    times(9).reverse().forEach(index => {
+      act(() => {
+        wrapper.setProps({ current: index });
+      });
+      wrapper.update();
+
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
