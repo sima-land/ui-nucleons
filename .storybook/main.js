f28e7ecde02bba88f7ff32faa1b64e268178b343
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2021-01-12 09:10:42 +0500

    #241
    - обновлены пакеты сборки, storybook
    - обновлена конфигурация storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: [
+    '../src/components/**/*.stories.@(js|jsx)',
+  ],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,35 +1,43 @@
-const path = require('path');
-
 module.exports = async ({ config }) => {
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            localIdentName: '[name]__[local]__[hash:hex:3]',
+  const rules = [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    {
+      test: /\.(css|scss)$/,
+      exclude: /node_modules/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              localIdentName: '[name]__[local]__[hash:hex:3]',
+            },
           },
         },
-      },
-      'sass-loader',
-    ],
-    include: path.resolve(__dirname, '../'),
-  });
-
-  config.module.rules.push({
-    test: /\.(woff|woff2|eot|ttf)$/,
-    use: [
-      {
-        loader: 'file-loader',
-        query: {
-          name: '[name].[ext]'
+        'sass-loader',
+      ],
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'file-loader',
+          query: {
+            name: '[name].[ext]'
+          }
         }
-      }
-    ],
-    include: path.resolve(__dirname, '../'),
-  });
+      ],
+    },
+  ];
 
-  return config;
+  return { ...config, module: { ...config.module, rules } };
 };
