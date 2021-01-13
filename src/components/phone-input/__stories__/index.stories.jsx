abed9787a0cba0f647e8d8be1249360e7106b452
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2021-01-13 12:57:08 +0500

    #242
    - перенесена функция определяющая страну по номеру телефона
    - добавлено определение страны по умолчанию в PhoneInput
    - добавлена логика получения/возврата значения сразу с кодом страны
    - обновлены тесты, stories

--- src/components/phone-input/__stories__/index.stories.jsx
+++ src/components/phone-input/__stories__/index.stories.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { PhoneInput } from '..';
-import { useState } from '@storybook/addons';
+
+const DemoForm = ({ initialValue = '' }) => {
+  const [value, setValue] = useState(initialValue);
+  const [error, setError] = useState(null);
+
+  return (
+    <PhoneInput
+      value={value}
+      caption={error}
+      failed={Boolean(error)}
+      onFocus={() => {
+        setError(null);
+      }}
+      onBlur={(e, { cleanValue, ready }) => {
+        setValue(cleanValue);
+        !ready && setError('Поле не заполнено');
+      }}
+      onCountrySelect={() => {
+        setValue('');
+        setError(null);
+      }}
+    />
+  );
+};
 
 storiesOf('PhoneInput', module)
-  .add('Usage', () => {
-    const [value, setValue] = useState('');
-    const [error, setError] = useState(null);
+  .add('Usage', () => (
+    <div style={{ padding: 32 }}>
+      <h3>Пустое значение (Россия, по умолчанию)</h3>
+      <DemoForm initialValue='' />
 
-    return (
-      <div style={{ padding: 32 }}>
-        <PhoneInput
-          value={value}
-          caption={error}
-          failed={Boolean(error)}
-          onFocus={() => {
-            setError(null);
-          }}
-          onBlur={(e, { cleanValue, ready }) => {
-            setValue(cleanValue);
-            !ready && setError('Поле не заполнено');
-          }}
-          onCountrySelect={() => {
-            setValue('');
-            setError(null);
-          }}
-        />
-      </div>
-    );
-  });
+      <h3>Известная страна (Беларусь)</h3>
+      <DemoForm initialValue='375112223344' />
+
+      <h3>Известная страна (Казахстан)</h3>
+      <DemoForm initialValue='76005002040' />
+
+      <h3>Неизвестная страна (Другое)</h3>
+      <DemoForm initialValue='55566667777' />
+    </div>
+  ));
