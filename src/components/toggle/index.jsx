179d17d179cee183640f135647a7a4bbdf120bd1
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2021-02-16 09:13:47 +0500

    #259
    - переименован Switch в Toggle
    - доработан Toggle

--- src/components/toggle/index.jsx
+++ src/components/toggle/index.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
-import classes from './switch.scss';
+import styles from './toggle.scss';
 
-const cx = classnames.bind(classes);
+const cx = classnames.bind(styles);
 
 /**
  * Возвращает компонент стилизованного переключателя (input[type=checkbox]).
  * @param {Object} props Свойства.
  * @param {boolean} [props.checked=props.defaultChecked] Отмечен ли переключатель.
+ * @param {boolean} [props.disabled] Отключен ли переключатель.
  * @param {string} [props.className] CSS-классы корневого компонента.
  * @param {boolean} [props.defaultChecked=false] Отмечен ли переключатель по умолчанию.
  * @param {Object} [props.inputProps] Свойства внутреннего элемента input.
@@ -17,67 +18,36 @@ const cx = classnames.bind(classes);
  * @param {import('react').RefObject | Function} [props.inputRef] Ref внутреннего элемента input.
  * @return {ReactElement} Компонент стилизованного переключателя.
  */
-const Switch = ({
+export const Toggle = forwardRef(({
   checked,
   className,
   defaultChecked,
   inputProps,
-  inputRef,
   onChange,
   style,
-}) => (
-  <label className={cx('switch-root', className)} style={style}>
+  disabled,
+}, ref) => (
+  <label className={cx('root', className)} style={style}>
     <input
       {...inputProps}
+      disabled={disabled}
       checked={checked}
-      className={cx('switch-input')}
+      className={cx('input')}
       defaultChecked={defaultChecked}
       onChange={onChange}
-      ref={inputRef}
+      ref={ref}
       type='checkbox'
     />
     <span className={cx('switch')} />
   </label>
-);
+));
 
-Switch.propTypes = {
-  /**
-   * Отмечен ли переключатель по умолчанию.
-   */
+Toggle.propTypes = {
   defaultChecked: PropTypes.bool,
-
-  /**
-   * Отмечен ли переключатель.
-   */
   checked: PropTypes.bool,
-
-  /**
-   * Сработает при смене состояния.
-   */
+  disabled: PropTypes.bool,
   onChange: PropTypes.func,
-
-  /**
-   * Ref внутреннего элемента input.
-   */
-  inputRef: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-
-  /**
-   * Свойства внутреннего элемента input.
-   */
   inputProps: PropTypes.object,
-
-  /**
-   * CSS-классы корневого элемента.
-   */
   className: PropTypes.string,
-
-  /**
-   * Стили корневого элемента.
-   */
   style: PropTypes.string,
 };
-
-export default Switch;
