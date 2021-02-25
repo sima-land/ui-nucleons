5adfa7601eac53a7597596d0796f35ea0099a135
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2021-02-25 09:05:44 +0500

    #277
    - добавлен компонент иконки файла

---
+++ src/components/file-icon/__stories__/index.stories.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FileIcon } from '..';
+
+export default {
+  title: 'FileIcon',
+  component: FileIcon,
+  parameters: {
+    layout: 'padded',
+  },
+};
+
+export const Primary = () => (
+  <div style={{ display: 'flex' }}>
+    {[
+      'doc',
+      'xls',
+      'pdf',
+      'jpg',
+      'xml',
+      'zip',
+      'unk',
+    ].map(fileType => (
+      <div key={fileType} style={{ marginRight: 12 }}>
+        <FileIcon type={fileType} />
+      </div>
+    ))}
+  </div>
+);
