d30b6eb2db62e8f01c927e66b581db643b7d9695
Павел Ротанов <rotanov_ps@ekb.sima-land.ru>
2021-02-26 10:11:46 +0500

    #274 Компонент Tabs изменен в соотвествии с новыми гайдами

--- src/components/tabs/__stories__/tabs.stories.js
+++ src/components/tabs/__stories__/tabs.stories.js
@@ -1,95 +1,48 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import Tabs from '../';
 
-const items = ['Tab 1', 'Tab 2', 'Tab 3'];
-const customItems = [
-  { id: 1, name: 'first' },
-  { id: 2, name: 'second' },
-  { id: 3, name: 'third' },
+const items = [
+  { name: 'Active', active: true },
+  { name: 'Inactive' },
+  { name: 'Inactive' },
+  { name: 'Disabled', disabled: true },
 ];
-const style = {
-  padding: '15px 25px',
+
+const Template = args => (
+  <div style={{ padding: '0 32px' }}>
+    <p>Базовый вид</p>
+    <Tabs {...args} />
+    <br />
+    <p>С узким зазором</p>
+    <Tabs {...args} gapSize='s' />
+    <br />
+    {args.type !== 'round' && (
+      <>
+        <p>С подчеркиванием</p>
+        <Tabs {...args} underline />
+        <br />
+      </>
+    )}
+    <p>На ширину контейнера</p>
+    <Tabs {...args} stretch />
+  </div>
+);
+export const TextView = Template.bind({});
+export const RoundView = Template.bind({});
+
+TextView.storyName = 'Текстовые табы';
+TextView.args = {
+  items,
+  getItemName: ({ name }) => name,
 };
-const grayStyle = {
-  ...style,
-  background: '#d1d2d6',
-};
-const itemStyle = {
-  width: '70px',
-  textAlign: 'center',
-};
-const tabHeaderStyle = {
-  borderBottom: '1px solid #e8b6cf',
+
+RoundView.storyName = 'Табы-кнопки';
+RoundView.args = {
+  ...TextView.args,
+  type: 'round',
 };
 
-class SelectableTabs extends React.Component {
-  state = {
-    selected: 'Tab 1',
-  };
-  isSelectedItem = item => item === this.state.selected;
-  onSelectItem = item => this.setState({ selected: item });
-
-  render () {
-    return (
-      <Tabs items={items} isSelectedItem={this.isSelectedItem} onSelectItem={this.onSelectItem} />
-    );
-  }
-}
-
-class CustomTabs extends React.Component {
-  state = {
-    selected: 3,
-  };
-  renderItem = item => (
-    <div style={itemStyle}>
-      <div style={tabHeaderStyle}>Tab #{item.id}</div>
-      <div>{item.name}</div>
-    </div>
-  );
-  isSelectedItem = item => item.id === this.state.selected;
-  onSelectItem = item => this.setState({ selected: item.id });
-
-  render () {
-    return (
-      <Tabs
-        items={customItems}
-        isSelectedItem={this.isSelectedItem}
-        onSelectItem={this.onSelectItem}
-        renderItem={this.renderItem}
-      />
-    );
-  }
-}
-
-storiesOf('Tabs', module)
-  .add('default', () => (
-    <div>
-      <div style={style}>
-        <Tabs items={items} />
-      </div>
-      <div style={grayStyle}>
-        <Tabs items={items} />
-      </div>
-    </div>
-  ))
-  .add('selectable tabs', () => (
-    <div>
-      <div style={style}>
-        <SelectableTabs />
-      </div>
-      <div style={grayStyle}>
-        <SelectableTabs />
-      </div>
-    </div>
-  ))
-  .add('custom tabs', () => (
-    <div>
-      <div style={style}>
-        <CustomTabs />
-      </div>
-      <div style={grayStyle}>
-        <CustomTabs />
-      </div>
-    </div>
-  ));
+export default {
+  title: 'Tabs',
+  component: Tabs,
+};
