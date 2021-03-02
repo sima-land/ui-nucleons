1ce126e1754ea9d1a681c823ab8fd1d2f85f399e
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2021-03-02 16:55:05 +0500

    #288
    - переписано create-container на TS
    - обновлен readme

--- src/components/helpers/create-container.ts
+++ src/components/helpers/create-container.ts
@@ -1,38 +1,36 @@
-import isFunction from 'lodash/isFunction';
+type Observer<T> = (prev: T | undefined, next: T | undefined) => void;
 
 /**
  * Фабрика контейнеров для произвольных значений.
  * @param {*} initialValue Начальное значение в контейнере.
  * @return {{ set: Function, get: Function, setObserver: Function }} Контейнер.
  */
-const createContainer = initialValue => {
-  let value = initialValue;
-  let observe;
+const createContainer = <T = any>(initialValue?: T) => {
+  let value: T | undefined = initialValue;
+  let observe: Observer<T> | null;
 
   /**
    * Геттер значения.
    * @return {*} Значение.
    */
-  const get = () => value;
+  const get = (): T | undefined => value;
 
   /**
    * Сеттер значения.
    * @param {*} newValue Новое значение.
    */
-  const set = newValue => {
-    const oldValue = value;
+  const set = (newValue: T) => {
+    const oldValue: T | undefined = value;
     value = newValue;
 
-    !Object.is(newValue, oldValue)
-      && isFunction(observe)
-      && observe(newValue, oldValue);
+    !Object.is(newValue, oldValue) && observe?.(newValue, oldValue);
   };
 
   /**
    * Сеттер функции-наблюдателя за изменением значения.
    * @param {Function} newObserve Функция-наблюдатель.
    */
-  const setObserver = newObserve => {
+  const setObserver = (newObserve: Observer<T> | null) => {
     observe = newObserve;
   };
 
