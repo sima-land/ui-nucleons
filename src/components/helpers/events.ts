6ec153b12e0926fb6920cc3e92b3f7c293ff6ef0
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2021-03-03 09:13:09 +0500

    #291
    - переписан helpers/events на TS

---
+++ src/components/helpers/events.ts
@@ -0,0 +1,26 @@
+import Point from './point';
+
+/**
+ * Проверяет, является ли событие событием нажатия основной кнопки мыши.
+ * @param e Событие.
+ * @return Является ли.
+ */
+export const isMainMouseButton = (e: any) => e?.button === 0;
+
+/**
+ * Проверяет, является ли переданное событие touch-событием.
+ * @param e Событие.
+ * @return Является ли.
+ */
+export const isTouchEvent = (e: any): e is TouchEvent => Boolean(e?.touches);
+
+/**
+ * Берет данные позиции из события.
+ * @param e Touch- или Mouse-событие.
+ * @return Данные позиции.
+ */
+export const getEventClientPos = (e: MouseEvent | TouchEvent) => {
+  const source = isTouchEvent(e) ? e.touches[0] : e;
+
+  return Point(source?.clientX, source?.clientY);
+};
