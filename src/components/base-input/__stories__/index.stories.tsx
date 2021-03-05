53f2382a15aef625d4067fffb76e3c60f06f2994
дима петров <petrov_dm@ekb.sima-land.ru>
2021-03-05 12:12:27 +0500

    feat(BaseInput): Компонент переведён на TS.

---
+++ src/components/base-input/__stories__/index.stories.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { BaseInput } from '..';
+
+export default {
+  title: 'BaseInput',
+  component: BaseInput,
+  parameters: {
+    layout: 'padded',
+  },
+};
+
+export const Primary = () => {
+  const styles = {
+    border: '1px solid #000',
+    display: 'flex',
+    width: 200,
+    padding: 8,
+  };
+
+  const longValue = [
+    'Lorem ipsum dolor sit amet consectetur,',
+    'adipisicing elit. Distinctio maxime at tempora',
+    'adipisci placeat odio omnis laudantium cumque.',
+    'Omnis, accusamus?',
+  ].join(' \n');
+
+  return (
+    <>
+      <h3>Regular</h3>
+      <BaseInput
+        defaultValue='Text'
+        style={styles}
+      />
+
+      <div style={{ height: 32 }} />
+
+      <h3>Multiline</h3>
+      <BaseInput
+        multiline
+        defaultValue={longValue}
+        style={styles}
+      />
+    </>
+  );
+};
+
+export const RestPlaceholder = () => {
+  const [value, setValue] = useState('');
+
+  return (
+    <>
+      <h3>Введите 10 цифр</h3>
+
+      <BaseInput
+        value={value}
+        onChange={e => setValue(e.target.value.replace(/\D/g, '').slice(0, 10))}
+        restPlaceholder={{ value: '9'.repeat(10).slice(value.length) }}
+        style={{
+          padding: 16,
+          width: 240,
+          border: '1px solid #aaa',
+          '--placeholder-color': '#aaa',
+        }}
+      />
+    </>
+  );
+};
---
+++ src/components/base-input/index.tsx
@@ -0,0 +1,95 @@
+import React, { useRef, useEffect, useImperativeHandle, forwardRef } from 'react';
+import classnames from 'classnames/bind';
+import classes from './base-input.scss';
+import { fitElementHeight } from '../helpers/fit-element-height';
+import { isString } from 'lodash';
+
+const cx = classnames.bind(classes);
+
+interface RestPlaceholder {
+  value: string
+  shiftValue?: string
+}
+
+interface Styles extends React.CSSProperties {
+  '--placeholder-color'?: string
+}
+
+interface CustomProps {
+  multiline?: boolean
+  restPlaceholder?: string | RestPlaceholder
+  style?: Styles
+}
+
+export type Props = CustomProps & React.HTMLProps<HTMLTextAreaElement> & React.HTMLProps<HTMLInputElement>
+
+/**
+ * Компонент поля ввода.
+ * @param props Свойства. Поддерживаются свойства элемента input.
+ * @param props.multiline Нужно ли выводить textarea вместо input.
+ * @param props.restPlaceholder Остаточный placeholder (выводится после введенного значения).
+ * @return Компонент поля ввода.
+ */
+export const BaseInput = forwardRef<HTMLTextAreaElement | HTMLInputElement | undefined, Props>(function BaseInput ({
+  className,
+  disabled,
+  multiline,
+  onInput,
+  restPlaceholder: restPlaceholderProp,
+  rows = 1,
+  style,
+  value,
+  ...props
+}, ref) {
+  const Element = multiline ? 'textarea' : 'input' as typeof multiline extends true ? 'textarea' : 'input';
+  const inputRef = useRef<HTMLTextAreaElement | HTMLInputElement>();
+
+  useImperativeHandle(ref, () => inputRef.current);
+
+  useEffect(() => {
+    multiline && inputRef.current && fitElementHeight({
+      target: inputRef.current,
+    });
+  }, []);
+
+  const restPlaceholder: RestPlaceholder = isString(restPlaceholderProp)
+    ? { shiftValue: value, value: restPlaceholderProp }
+    : { shiftValue: value, ...restPlaceholderProp as any };
+
+  return (
+    <div
+      style={style}
+      className={cx(
+        'reset',
+        'root',
+        disabled && 'disabled',
+        className
+      )}
+    >
+      {!multiline && Boolean(restPlaceholder.value) && (
+        <span aria-hidden className={cx('fake-text')}>
+          <span className={cx('invisible-value')}>{restPlaceholder.shiftValue}</span>
+          <span className={cx('placeholder')}>{restPlaceholder.value}</span>
+        </span>
+      )}
+      <Element
+        {...props}
+        rows={multiline ? rows : undefined}
+        ref={inputRef as any}
+        disabled={disabled}
+
+        // ВАЖНО: не даем возможности задать стили/классы именно для этого элемента
+        className={cx(
+          'field',
+          'reset',
+          multiline && 'multiline'
+        )}
+        value={value}
+        onInput={(event: any) => {
+          multiline && fitElementHeight(event);
+          onInput?.(event as any);
+        }}
+      />
+    </div>
+  );
+});
