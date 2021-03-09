fce6944ee0992a39f80bd52c383f1800a084785f
дима петров <petrov_dm@ekb.sima-land.ru>
2021-03-09 10:18:14 +0500

    feat(Alert): Переведены на TS компоненты Alert, NavBar и CleanButtons.

---
+++ src/components/alert/__stories__/index.stories.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Box from '../../box';
+import Text from '../../text';
+import Clean from '../../clean-buttons';
+import Alert from '..';
+
+export default {
+  title: 'Alert',
+  component: Alert,
+  parameters: {
+    layout: 'padded',
+  },
+};
+
+export const Primary = () => (
+  <Alert
+    children={(
+      <Box marginTop={24} marginBottom={16} display='flex' justifyContent='center'>
+        <Text align='center'>
+          Перенести товары
+          <br />
+          из списка в корзину?
+        </Text>
+      </Box>
+    )}
+    footer={(
+      <Clean.Group>
+        <Clean.Button>Отмена</Clean.Button>
+        <Clean.Button>Ок</Clean.Button>
+      </Clean.Group>
+    )}
+  />
+);
+
+export const WIthHeader = () => (
+  <Alert
+    title='Тестовый диалог'
+    withDivideNavBar
+    children={(
+      <Box marginY={16}display='flex' justifyContent='center'>
+        <Text align='center'>
+          Перенести товары
+          <br />
+          из списка в корзину?
+        </Text>
+      </Box>
+    )}
+    footer={(
+      <Clean.Group>
+        <Clean.Button>Отмена</Clean.Button>
+        <Clean.Button>Ок</Clean.Button>
+      </Clean.Group>
+    )}
+  />
+);
---
+++ src/components/clean-buttons/index.tsx
@@ -0,0 +1,72 @@
+import React, { Children, cloneElement } from 'react';
+import classnames from 'classnames/bind';
+import classes from './clean-buttons.scss';
+
+type Size = 's' | 'm'
+
+interface CustomButtonProps {
+  asLink?: boolean
+  size?: Size
+}
+
+export type ButtonProps = CustomButtonProps
+& Omit<React.HTMLProps<HTMLAnchorElement>, 'size'>
+& Omit<React.HTMLProps<HTMLButtonElement>, 'size'>
+
+export interface Props {
+  children?: React.ReactElement<ButtonProps>[]
+  size?: Size
+}
+
+const cx = classnames.bind(classes);
+
+/**
+ * Компонент группы прозрачных кнопок.
+ * @param props Свойства.
+ * @param props.size Содержимое.
+ * @return Элемент.
+ */
+const CleanGroup: React.FC<Props> = ({ children, size = 'm' }) => (
+  <div className={cx('clean-group')}>
+    {Children.map(
+      children,
+      item => item && item.type === CleanButton
+        ? cloneElement(item, { size })
+        : item
+    )}
+  </div>
+);
+
+/**
+ * Компонент прозрачной кнопки.
+ * @param props Свойства. Поддерживаются свойства элементов button/a (зависит от того, передан ли asLink).
+ * @param props.children Содержимое.
+ * @param props.size Размер.
+ * @param props.href Адрес ссылки.
+ * @param props.asLink Нужно ли выводить кнопку как ссылку.
+ * @return Элемент.
+ */
+const CleanButton: React.FC<ButtonProps> = ({
+  size = 'm',
+  href,
+  asLink = Boolean(href),
+  ...restProps
+}) => {
+  const Container = asLink ? 'a' : 'button';
+
+  return (
+    <Container
+      className={cx('clean-button', `size-${size}`)}
+      href={href}
+      {...restProps}
+      type='button'
+    />
+  );
+};
+
+export const Clean = {
+  Group: CleanGroup,
+  Button: CleanButton,
+};
+
+export default Clean;
