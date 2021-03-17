b9e051d79129620e10ea3da6dd50f5f8215119b5
дима петров <petrov_dm@ekb.sima-land.ru>
2021-03-17 16:51:35 +0500

    feat(Modal): Компонент переведён на TS, удалён устаревший функционал.

---
+++ src/components/modal/utils.ts
@@ -0,0 +1,25 @@
+import { useRef, useCallback } from 'react';
+
+/**
+ * @param callback Сработает при закрытии.
+ * @return Свойства для закрывающего элемента.
+ */
+export const useCloseHandler = (callback: () => void) => {
+  const mouseDownTarget = useRef<HTMLElement>();
+
+  const onMouseDown = useCallback(({ button, target }) => {
+    if (button === 0) {
+      mouseDownTarget.current = target;
+    }
+  }, []);
+
+  const onMouseUp = useCallback(({ target, currentTarget, button }) => {
+    callback
+      && button === 0
+      && target === currentTarget
+      && currentTarget === mouseDownTarget.current
+      && callback();
+  }, [callback]);
+
+  return { onMouseUp, onMouseDown };
+};
--- src/components/top-bar/__test__/test-props.tsx
+++ src/components/top-bar/__test__/test-props.tsx
@@ -5,16 +5,6 @@ import PersonSVG from '@dev-dep/ui-quarks/icons/24x24/Stroked/person.js';
 
 const longText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ad, provident aspernatur dolore. ';
 
-/**
- * Создает обработчик клика.
- * @param {string} buttonPosition Название позиции кнопки.
- * @return {Function} Обработчик клика.
- */
-const createClickHandler = buttonPosition => () => {
-  // eslint-disable-next-line no-console
-  console.log(`The click to the ${buttonPosition} button was handled`);
-};
-
 export const sizes = ['s', 'm', 'l'];
 
 export const shortTitles = { title: 'Modal title', subtitle: 'Modal subtitle' };
@@ -24,22 +14,18 @@ export const longTitles = { title: longText.repeat(2), subtitle: longText.repeat
 export const startButtons = {
   start: {
     icon: <ArrowLeftSVG />,
-    onClick: createClickHandler('start'),
   },
   startSecondary: {
     icon: <PersonSVG />,
-    onClick: createClickHandler('start secondary'),
   },
 };
 
 export const endButtons = {
   end: {
     icon: <CrossSVG />,
-    onClick: createClickHandler('end'),
   },
   endSecondary: {
     icon: <PersonSVG />,
-    onClick: createClickHandler('end secondary'),
   },
 };
 
