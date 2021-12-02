8cff200ec1ff805fdb72d5f1a8200185d3851f4d
krutoo <dima.krutoo@yandex.ru>
2021-12-02 18:05:20 +0500

    #85
    - Chips: формирование содержимого переделано на слоты
    - правки тестов, stories

--- src/chips/index.tsx
+++ src/chips/index.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
+import React, { Children, isValidElement } from 'react';
+import { ChipsItem } from './item';
 import classnames from 'classnames/bind';
-import CrossSVG from '@sima-land/ui-quarks/icons/16x16/Stroked/cross';
 import styles from './chips.module.scss';
-
-interface ChipsItem {
-  href?: string;
-  children?: React.ReactNode;
-  withCross?: boolean;
-  target?: string;
-  [x: string]: any;
-}
 
 export interface ChipsProps extends React.HTMLAttributes<HTMLDivElement> {
   /** Элементы списка. */
-  items: ChipsItem[];
-
-  /** Сработает при клике на элемент. */
-  onItemClick?: (item: ChipsItem, event: React.MouseEvent<HTMLAnchorElement>) => void;
-
-  /** Определит, отмечен ли переданный элемент. */
-  isItemChecked?: (item: ChipsItem, index: number) => boolean;
+  children: React.ReactNode;
 
   /** Идентификатор для систем автоматизированного тестирования. */
   'data-testid'?: string;
@@ -33,31 +19,14 @@ const cx = classnames.bind(styles);
  * @return Элемент.
  */
 export const Chips = ({
-  items,
-  onItemClick,
-  isItemChecked,
+  children,
   className,
   'data-testid': testId = 'chips',
   ...restProps
 }: ChipsProps) => (
   <div {...restProps} className={cx('list', className)} data-testid={testId}>
-    {items.map((item, index) => {
-      const { href, target, children, withCross } = item;
-
-      return (
-        <a
-          href={href}
-          role={href ? undefined : 'button'}
-          key={index}
-          className={cx('item', isItemChecked?.(item, index) && 'checked')}
-          target={target}
-          onClick={e => onItemClick?.(item, e)}
-          data-testid='chips:item'
-        >
-          <span className={styles.text}>{children}</span>
-          {withCross && <CrossSVG className={cx('cross-svg')} />}
-        </a>
-      );
-    })}
+    {Children.toArray(children).filter(c => isValidElement(c) && c.type === ChipsItem)}
   </div>
 );
+
+Chips.Item = ChipsItem;
