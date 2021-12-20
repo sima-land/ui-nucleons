57ebdfd7b09e85f62a5fdd8256933e7e0c103f16
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2021-12-20 13:55:31 +0500

    #89
    - добален компонент StrokedSVG
    - добавлены тесты, stories

---
+++ src/stroked-svg/index.tsx
@@ -0,0 +1,44 @@
+import React, { forwardRef } from 'react';
+import { COLORS } from '../colors';
+import classNames from 'classnames';
+import styles from './index.module.scss';
+
+export interface StrokedSVGProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
+  /** Компонент, выводящий svg-элемент. */
+  component: React.ComponentType<React.SVGAttributes<SVGSVGElement>>;
+
+  /** Ширина внешней обводки. */
+  stroke?: string;
+
+  /** Цвет внешней обводки. */
+  strokeWidth?: number;
+
+  /** Цвет заливки. */
+  fill?: string;
+}
+
+/**
+ * Выводит иконку с внешней обводкой.
+ * @param props Свойства.
+ * @return Элемент.
+ */
+export const StrokedSVG = forwardRef<HTMLDivElement, StrokedSVGProps>(
+  (
+    {
+      component: Tag,
+      fill,
+      stroke = COLORS.get('gray4'),
+      strokeWidth = 1,
+      className,
+      ...restProps
+    },
+    ref,
+  ) => (
+    <div ref={ref} className={classNames(styles.root, className)} {...restProps}>
+      <Tag stroke={stroke} strokeWidth={`${strokeWidth * 2}px`} className={styles.stroke} />
+      <Tag fill={fill} className={styles.svg} />
+    </div>
+  ),
+);
+
+StrokedSVG.displayName = 'StrokedSVG';
