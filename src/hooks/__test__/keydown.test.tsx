dcaf08299750c84b90649b7384769f09a09894fe
krutoo <dima.krutoo@yandex.ru>
2022-03-16 09:52:52 +0500

    #125
    - useKeyDown: добавлена передача события в callback
    - useKeyDown: добавлены тесты
    - useKeyDown: добавлены stories

--- src/hooks/__test__/keydown.test.tsx
+++ src/hooks/__test__/keydown.test.tsx
@@ -1,63 +1,50 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { render, fireEvent, createEvent } from '@testing-library/react';
 import { useKeydown } from '../keydown';
-import { mount } from 'enzyme';
 
 describe('useKeydown', () => {
-  let container: HTMLDivElement;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container);
-  });
-
   const TestComponent = ({ spy }: { spy?: () => void }) => {
     useKeydown('Escape', spy);
 
     return <div />;
   };
 
-  const keyDown = (key: string) => new KeyboardEvent('keydown', { bubbles: true, key });
-
   it('should works properly', () => {
     const spy = jest.fn();
 
-    act(() => {
-      render(<TestComponent spy={spy} />, container);
-    });
+    const { unmount } = render(<TestComponent spy={spy} />);
 
     expect(spy).toHaveBeenCalledTimes(0);
 
-    act(() => {
-      document.dispatchEvent(keyDown('Enter'));
-    });
+    fireEvent.keyDown(document, { key: 'Enter' });
     expect(spy).toHaveBeenCalledTimes(0);
 
-    act(() => {
-      document.dispatchEvent(keyDown('Escape'));
-    });
+    fireEvent.keyDown(document, { key: 'Escape' });
     expect(spy).toHaveBeenCalledTimes(1);
 
     jest.spyOn(document, 'removeEventListener');
-    const wrapper = mount(<TestComponent spy={spy} />);
-    wrapper.unmount();
+    unmount();
     expect(document.removeEventListener).toHaveBeenCalledTimes(1);
   });
 
-  it('should not throw error if callback is not a func', () => {
-    act(() => {
-      render(<TestComponent spy={undefined} />, container);
-    });
+  it('should not throw error if callback is not a function', () => {
+    render(<TestComponent spy={undefined} />);
 
     expect(() => {
-      act(() => {
-        document.dispatchEvent(keyDown('Escape'));
-      });
+      fireEvent.keyDown(document, { key: 'Escape' });
     }).not.toThrowError();
+  });
+
+  it('callback arugment must be an event', () => {
+    const spy = jest.fn();
+
+    render(<TestComponent spy={spy} />);
+
+    const event = createEvent.keyDown(document, { key: 'Escape' });
+
+    expect(spy).toBeCalledTimes(0);
+    fireEvent(document, event);
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toBeCalledWith(event);
   });
 });
