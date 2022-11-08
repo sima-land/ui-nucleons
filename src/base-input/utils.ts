e8180aa19a1f1e721fc654062ecc5200a1497f2f
krutoo <dima.krutoo@yandex.ru>
2022-11-08 11:39:52 +0500

    #194
    - правки типов

--- src/base-input/utils.ts
+++ src/base-input/utils.ts
@@ -3,7 +3,7 @@
  * @param source Объект.
  * @return Копия без свойства multiline.
  */
-export function omitMultiline<T extends Record<string, unknown>>(source: T): Omit<T, 'multiline'> {
+export function omitMultiline<T extends Record<string, any>>(source: T): Omit<T, 'multiline'> {
   const next = { ...source };
 
   delete next.multiline;
