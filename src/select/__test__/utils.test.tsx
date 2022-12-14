7fd4b2cf4eb5010c9cc6e08030c626e434c2a30c
krutoo <dima.krutoo@yandex.ru>
2022-12-14 12:04:04 +0500

    #217
    - select: добавлены тесты

---
+++ src/select/__test__/utils.test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent, render } from '@testing-library/react';
+import React, { useContext } from 'react';
+import { SelectOpenerBinding } from '../types';
+import { SelectContext } from '../utils';
+
+describe('SelectContext', () => {
+  it('should contain defaults', () => {
+    const spy = jest.fn((bind: SelectOpenerBinding) => bind);
+
+    function TestComponent() {
+      const bind = useContext(SelectContext);
+
+      spy(bind);
+
+      return (
+        <div data-testid='test-stub' onMouseDown={bind.onMouseDown} onKeyDown={bind.onKeyDown}>
+          Hello
+        </div>
+      );
+    }
+
+    const { getByTestId } = render(<TestComponent />);
+
+    // check defaults
+    expect(spy).toBeCalledTimes(1);
+    expect(typeof spy.mock.calls[0][0].onKeyDown === 'function').toBe(true);
+    expect(typeof spy.mock.calls[0][0].onMouseDown === 'function').toBe(true);
+
+    // call default callbacks
+    expect(() => {
+      fireEvent.mouseDown(getByTestId('test-stub'));
+      fireEvent.keyDown(getByTestId('test-stub'));
+    }).not.toThrow();
+  });
+});
