eb74874aae2c932fcc1b4d2ed2617e6a7a829673
krutoo <dima.krutoo@yandex.ru>
2023-01-16 18:21:48 +0500

    #238
    - InfoText: добавлен проп iconRef

--- src/info-text/index.tsx
+++ src/info-text/index.tsx
@@ -1,4 +1,4 @@
-import React, { Children, CSSProperties, Fragment, ReactNode, SyntheticEvent } from 'react';
+import React, { Children, CSSProperties, Fragment, ReactNode, Ref, SyntheticEvent } from 'react';
 import InfoSVG from '@sima-land/ui-quarks/icons/16x16/Stroked/information';
 import classNames from 'classnames/bind';
 import styles from './info-text.module.scss';
@@ -7,6 +7,7 @@ export interface InfoTextProps {
   children?: ReactNode;
   className?: string;
   style?: CSSProperties;
+  iconRef?: Ref<SVGSVGElement>;
   onIconClick?: (event: SyntheticEvent<SVGSVGElement>) => void;
   'data-testid'?: string;
 }
@@ -23,6 +24,7 @@ export function InfoText({
   children,
   style,
   className,
+  iconRef,
   onIconClick,
   'data-testid': testId = 'info-text',
 }: InfoTextProps) {
@@ -55,6 +57,7 @@ export function InfoText({
                         }
                       },
                     })}
+                    ref={iconRef}
                     className={cx('icon', onIconClick && 'button')}
                     onClick={onIconClick}
                     data-testid='info-text:icon'
