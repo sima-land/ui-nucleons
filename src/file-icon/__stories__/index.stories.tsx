3ff28e644e3fd929b3104b1c429557ec895e6995
krutoo <dima.krutoo@yandex.ru>
2023-08-28 16:19:26 +0500

    #314
    - file-icon: переработан компонент (minor)
    - file-icon: добавлены e2e-тесты

--- src/file-icon/__stories__/index.stories.tsx
+++ src/file-icon/__stories__/index.stories.tsx
@@ -1,4 +1,4 @@
-import { FileIcon } from '@sima-land/ui-nucleons/file-icon';
+import { FileIcon, FileIconStyle } from '@sima-land/ui-nucleons/file-icon';
 
 export default {
   title: 'common/FileIcon',
@@ -10,14 +10,32 @@ export default {
 
 export function Primary() {
   return (
-    <div style={{ display: 'flex' }}>
-      {['doc', 'xls', 'pdf', 'jpg', 'xml', 'zip', 'unk'].map(fileType => (
-        <div key={fileType} style={{ marginRight: 12 }}>
-          <FileIcon type={fileType as any} />
-        </div>
-      ))}
+    <div style={{ display: 'flex', gap: '12px' }}>
+      <FileIcon width={48} height={48} type='doc' />
+      <FileIcon width={48} height={48} type='xls' />
+      <FileIcon width={48} height={48} type='pdf' />
+      <FileIcon width={48} height={48} type='jpg' />
+      <FileIcon width={48} height={48} type='xml' />
+      <FileIcon width={48} height={48} type='zip' />
+      <FileIcon width={48} height={48} type='unknown' />
     </div>
   );
 }
 
 Primary.storyName = 'Простой пример';
+
+export function CustomColors() {
+  const style: FileIconStyle = {
+    '--file-icon-primary-color': '#3264a8',
+  };
+
+  return (
+    <div style={{ display: 'flex', gap: '12px' }}>
+      <FileIcon style={style} width={48} height={48} type='doc' />
+      <FileIcon style={style} width={48} height={48} type='svg' typeDisplayed />
+      <FileIcon style={style} width={48} height={48} />
+    </div>
+  );
+}
+
+CustomColors.storyName = 'Свои цвета';
