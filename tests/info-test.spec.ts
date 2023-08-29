f41b7b6718b39192ff7df960e8e266662495c794
krutoo <dima.krutoo@yandex.ru>
2023-08-29 10:48:43 +0500

    #314
    - info-text: добавлены e2e-тесты

---
+++ tests/info-test.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+import { TestUtils } from './utils';
+
+class Here extends TestUtils {
+  infoText() {
+    return this.page.getByTestId('info-text');
+  }
+}
+
+const here = new Here().register();
+
+test('info text should be on page', async ({ page }) => {
+  await page.goto('/iframe.html?args=&id=common-infotext--primary');
+
+  await expect(here.infoText()).toHaveCount(1);
+  await expect(here.page).toHaveScreenshot();
+});
