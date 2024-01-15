70302e5ccab2ca73bd23a15b9b99cfa2df68fe95
krutoo <dima.krutoo@yandex.ru>
2024-01-15 15:13:43 +0500

    #401
    - правки сборки сторибука

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+// данный конфиг нужен исключительно для Storybook
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
+  ],
+};
