4a93f1c1a0fd8e89da2c52da8239228f55b008ec
Dmitry Petrov <dima.krutoo@yandex.ru>
2024-01-24 17:28:55 +0500

    Шаг 5 #401 Прибраться перед очередным релизом (#410)
    - добавлены точки входа для helpers и styling

---
+++ src/styling/index.ts
@@ -0,0 +1,30 @@
+export { InnerBorder } from './borders';
+
+export { color, hoverColor, bgColor } from './colors';
+
+export {
+  type Size,
+  type LineHeight,
+  type Weight,
+  SIZES,
+  LINE_HEIGHTS,
+  WEIGHTS,
+  size,
+  lineHeight,
+  weight,
+} from './fonts';
+
+export { BoxShadow } from './shadows';
+
+export { SmallRounds, MediumRounds } from './shapes';
+
+export {
+  marginBottom,
+  marginTop,
+  marginLeft,
+  marginRight,
+  paddingBottom,
+  paddingLeft,
+  paddingRight,
+  paddingTop,
+} from './sizes';
