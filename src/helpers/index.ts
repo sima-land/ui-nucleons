17af3600dfe7bf684ae54d85911e400f8afe39a3
krutoo <dima.krutoo@yandex.ru>
2024-01-24 17:07:05 +0500

    #401
    - добавлены точки входа для helpers и styling

---
+++ src/helpers/index.ts
@@ -0,0 +1,13 @@
+// ВАЖНО: только определенные утилиты являются публичными, они экспортируются здесь
+export { defineSlots } from './define-slots';
+export { findOffsetParent } from './find-offset-parent';
+export { formatDate, createDateFormatter } from './format-date';
+export { getDateIntervalData } from './get-date-interval-data';
+export { getDeclination } from './get-declination';
+export { getScrollParent } from './get-scroll-parent';
+export { isBrowser } from './is-browser';
+export { isFullyScrolled } from './is-fully-scrolled';
+export { isTouchDevice } from './is-touch-device';
+export { useLayer, LayerProvider } from './layer';
+export { on } from './on';
+export { scrollToChild } from './scroll-to-child';
