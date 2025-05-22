55f0ecbe6e38237ed8b6784e9de381f5810da67c
Denis Tolmachev <dentolmachevfe@gmail.com>
2025-05-22 15:10:00 +0500

    #12235 Доработать компонент FileIcon

--- src/file-icon/file-icon.tsx
+++ src/file-icon/file-icon.tsx
@@ -1,6 +1,7 @@
 import type { FileIconProps } from './types';
 import cx from 'classnames';
 import styles from './file-icon.m.scss';
+import { useMemo } from 'react';
 
 export const KNOWN_TYPES = new Set([
   'avi',
@@ -28,27 +29,27 @@ export const KNOWN_TYPES = new Set([
  */
 export function FileIcon({
   type = '',
-  isTypeVisible = typeof type === 'string' && KNOWN_TYPES.has(type.toLowerCase()),
+  isTypeVisible = Boolean(type && KNOWN_TYPES.has(type.toLowerCase())),
   ...svgProps
 }: FileIconProps) {
-  let placeholder = (
-    /* заглушка текста */
-    <g opacity='0.8'>
-      <path d='M9 22H23V24H9V22Z' fill='#fff' />
-      <path d='M9 18H19V20H9V18Z' fill='#fff' />
-    </g>
+  const placeholder = useMemo(
+    () =>
+      type === null ? (
+        /* иконка Stroked/Cross без обертки в виде <svg /> */
+        <path
+          d='M12.7071 4.70711C13.0976 4.31658 13.0976 3.68342 12.7071 3.29289C12.3166 2.90237 11.6834 2.90237 11.2929 3.29289L8 6.58579L4.70711 3.29289C4.31658 2.90237 3.68342 2.90237 3.29289 3.29289C2.90237 3.68342 2.90237 4.31658 3.29289 4.70711L6.58579 8L3.29289 11.2929C2.90237 11.6834 2.90237 12.3166 3.29289 12.7071C3.68342 13.0976 4.31658 13.0976 4.70711 12.7071L8 9.41421L11.2929 12.7071C11.6834 13.0976 12.3166 13.0976 12.7071 12.7071C13.0976 12.3166 13.0976 11.6834 12.7071 11.2929L9.41421 8L12.7071 4.70711Z'
+          fill='#9e9e9e'
+          transform='translate(8,12)'
+        />
+      ) : (
+        /* заглушка текста */
+        <g opacity='0.8'>
+          <path d='M9 22H23V24H9V22Z' fill='#fff' />
+          <path d='M9 18H19V20H9V18Z' fill='#fff' />
+        </g>
+      ),
+    [type],
   );
-
-  if (type === null) {
-    placeholder = (
-      /* иконка Stroked/Cross без обертки в виде <svg /> */
-      <path
-        d='M12.7071 4.70711C13.0976 4.31658 13.0976 3.68342 12.7071 3.29289C12.3166 2.90237 11.6834 2.90237 11.2929 3.29289L8 6.58579L4.70711 3.29289C4.31658 2.90237 3.68342 2.90237 3.29289 3.29289C2.90237 3.68342 2.90237 4.31658 3.29289 4.70711L6.58579 8L3.29289 11.2929C2.90237 11.6834 2.90237 12.3166 3.29289 12.7071C3.68342 13.0976 4.31658 13.0976 4.70711 12.7071L8 9.41421L11.2929 12.7071C11.6834 13.0976 12.3166 13.0976 12.7071 12.7071C13.0976 12.3166 13.0976 11.6834 12.7071 11.2929L9.41421 8L12.7071 4.70711Z'
-        fill='#9e9e9e'
-        transform='translate(8,12)'
-      />
-    );
-  }
 
   return (
     <svg width='32' height='32' viewBox='0 0 32 32' {...svgProps}>
@@ -64,7 +65,7 @@ export function FileIcon({
       <path
         opacity='0.24'
         d='M25.1667 9L21 9L26 15L26 9C25.5833 9 25.6269 9 25.1667 9Z'
-        fill='black'
+        fill='#000'
       />
 
       {type && isTypeVisible ? (
